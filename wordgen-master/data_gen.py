3f9853b844b78c56acc8664cacb16664fc8e481d
James Meyers <james.meyers212@gmail.com>
2019-12-10 14:24:23 -0500

    fixed w's

--- wordgen-master/data_gen.py
+++ wordgen-master/data_gen.py
@@ -17,7 +17,7 @@ def fill_matrix(dataset):
 
     dataset = dataset.lower()
 
-    accepted = list("abcdefghijklmnopqrstuvqxyz") + ['\n']
+    accepted = list("abcdefghijklmnopqrstuvqwxyz") + ['\n']
     for i in range(len(dataset)-1):
         # if (dataset[i+1] in accepted and dataset[i] in accepted):
         if dataset[i] in accepted:
