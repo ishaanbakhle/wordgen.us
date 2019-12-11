45c5247ce04b13badd2e1b3164cedc9176a805c7
James Meyers <james.meyers212@gmail.com>
2019-12-11 14:02:15 -0500

    no triple letters

--- wordgen/generator.py
+++ wordgen/generator.py
@@ -19,11 +19,13 @@ def generate(matrix, num_strings=10):
 
         while ((len(generated) < 3 or consts.get_char(nli) != '') and len(generated) < 11):
             next_let = np.matmul(matrix, next_let)
+            if len(generated) > 1 and generated[:-1] == generated[:-2] and generated[:-1] != 'o':
+                next_let[consts.get_ord(generated[:-1])] = 0
             next_let /= next_let.sum()
             nli = np.random.choice(list(consts.rang), p=next_let)
             next_let = [0 for i in consts.rang]
             next_let[nli] = 1
             generated += consts.get_char(nli)
 
-        return(generated)
         num_strings -= 1
+    return(generated)
